0eee47e0be65359bf2ada6b31119312b49866fe1
kayalraju <kayalraju003@gmail.com>
2026-02-03 17:23:40 +0530

    laravel

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('common.head')
+
+<body>
+
+ <!-- ======= Header ======= -->
+ @include('common.navbar')
+ <!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+
+
+  @yield('containt')
+
+
+
+  <!-- ======= Footer ======= -->
+ @include('common.footer')
